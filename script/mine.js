7f9b3679f092d3b2c002c9360b4a88457ed4f1b5
Rostislav086 <Rostislav.086@mail.ru>
2020-08-05 11:28:22 +0500

    init ricket

---
+++ script/mine.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var instance = new vidbg(
+  ".video",
+  {
+    mp4: "/video/world.webm", // URL or relative path to MP4 video
+    webm: "/video/world.webm", // URL or relative path to webm video
+    poster: "/img/poster.jpg", // URL or relative path to fallback image
+    overlay: false, // Boolean to display the overlay or not
+  }
+);
+
+var rellax = new Rellax('.rocket');
